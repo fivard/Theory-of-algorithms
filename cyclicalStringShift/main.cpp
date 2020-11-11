f742db84e650f38d0e838138240bd82a96aef8fc
Dmytryi <57447810+fivard@users.noreply.github.com>
2020-11-11 22:38:08 +0200

    [ DONE ]

--- cyclicalStringShift/main.cpp
+++ cyclicalStringShift/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> KMP(std::string s){
+    int n = s.length();
+    std::vector<int> prefix(n);
+    for (int i = 1; i < n; i++){
+        int countMatched = prefix[i-1];
+        while (countMatched > 0 && s[i] != s[countMatched])
+            countMatched = prefix[countMatched - 1];
+        if (s[i] == s[countMatched])
+            countMatched++;
+        prefix[i] = countMatched;
+    }
+    return prefix;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string mainText, _template;
+    std::cout << "Enter two examples\n";
+    std::cin >> mainText >> _template;
+
+    mainText += mainText;
+    std::vector<int> answer = KMP(_template + mainText);
+
+    int id = -1;
+    for (int i = 0; i < answer.size(); i++){
+        std::cout << answer[i] << " ";
+        if (answer[i] == _template.size()) {
+            id = i;
+        }
+    }
+
+
+    if (id == -1)
+        std::cout << "\nIsn't cyclic shifted string\n";
+    else
+        std::cout << "\nIs cyclic shifted string. Shift equal to " << id - _template.size() - answer[id] + 1;
+
     return 0;
 }
