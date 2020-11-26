c5fe73d5b6ae381585066286a5f0b40c0c93a034
Dmytryi <57447810+fivard@users.noreply.github.com>
2020-11-26 11:30:27 +0200

    [ ADDED ] naive check

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+int naive(std::string text, std::string pattern){
+    for (int i = 0; i < text.size() - pattern.size(); i++){
+        int j = 0;
+        while (text[i + j] == pattern[j] && j < pattern.size())
+            j++;
+        if (j == pattern.size())
+            return i;
+    }
+    return -1;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string text, pattern;
+    int shift;
+
+    std::cout << "Enter a text\n";
+    std::cin >> text;
+    std::cout << "Enter a pattern\n";
+    std::cin >> pattern;
+
+    std::cout << "Naive check: ";
+    shift = naive(text, pattern);
+    if (shift == -1)
+        std::cout << "No matches were found\n";
+    else
+        std::cout << "A match was found. Shift is " << shift << std::endl;
+    
     return 0;
 }
