b21c4b903f44ff562d546183435dc2de1d3076d5
Dmytryi <57447810+fivard@users.noreply.github.com>
2021-05-01 14:16:15 +0300

    new lab, algoritm Johnson

---
+++ semestr2/lab9(AlgorithmJohnson)/Graph.h
@@ -0,0 +1,21 @@
+//
+// Created by Acer on 01.05.2021.
+//
+
+#ifndef LAB9_ALGORITHMJOHNSON__GRAPH_H
+#define LAB9_ALGORITHMJOHNSON__GRAPH_H
+
+#include <iostream>
+#include <vector>
+
+#define data first
+#define edges edges
+
+template <class T>
+class Graph{
+    std::vector< std::pair<T, std::vector< std::pair<T, T> > >> graph;
+public:
+
+};
+
+#endif //LAB9_ALGORITHMJOHNSON__GRAPH_H
---
+++ semestr2/lab9(AlgorithmJohnson)/main.cpp
@@ -0,0 +1,6 @@
+#include "Graph.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
